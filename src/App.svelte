cb2252665979a09ef7407af07e10ea2d0efe7b63
Mehdi ELOUALY <92476321+meel-hd@users.noreply.github.com>
2023-06-21 16:42:57 +0100

    Add Hiragana and Katakana alphabet

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,74 @@
 <script lang="ts">
+  import Hiragana from "./lib/screens/Hiragana/index.svelte";
+  import Katakana from "./lib/screens/Katakana/index.svelte";
+
+  let selected: "Hira" | "Kata" | "Kanji" = "Hira";
+
+  function selectHira() {
+    selected = "Hira";
+  }
+  function selectKata() {
+    selected = "Kata";
+  }
+  function selectKanji() {
+    selected = "Kanji";
+  }
 </script>
 
 <main>
-  <h1>Japanese</h1>
+  <header>
+    <button id={selected == "Hira" ? "active" : ""} on:click={selectHira}
+      >Hiragana</button
+    >
+    <button id={selected == "Kata" ? "active" : ""} on:click={selectKata}
+      >Katakana</button
+    >
+    <button id={selected == "Kanji" ? "active" : ""} on:click={selectKanji}
+      >Kanji</button
+    >
+  </header>
+  {#if selected == "Hira"}
+    <Hiragana />
+  {:else if selected == "Kata"}
+    <Katakana />
+  {:else if selected == "Kanji"}
+    <h1>Coming Soon...</h1>
+  {/if}
 </main>
 
 <style>
+  main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+  header {
+    position: absolute;
+    top: 20px;
+    left: 40px;
+    border-radius: 999px;
+    padding: 6px 20px;
+    background-color: var(--light-primary);
+    display: flex;
+    gap: 10px;
+    z-index: 100;
+  }
+  button {
+    border-radius: 999px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 35px;
+    width: 100px;
+  }
+  button:hover {
+    background: white;
+    box-shadow: 3px 4px 3px #00000010;
+  }
+  #active {
+    background: white;
+    box-shadow: 3px 4px 3px #00000010;
+  }
 </style>
---
+++ src/lib/screens/Hiragana/index.svelte
@@ -0,0 +1,98 @@
+<script lang="ts">
+  import Hiragana from "./Hiragana.json";
+  let charIndex = 0;
+  function next() {
+    if (charIndex < 45) {
+      charIndex++;
+    } else {
+      charIndex = 0;
+    }
+  }
+  function back() {
+    if (charIndex == 0) {
+      charIndex = 45;
+    } else {
+      charIndex--;
+    }
+  }
+</script>
+
+<main>
+  <h1 id="char">{Hiragana[charIndex].letter}</h1>
+  <div id="example">
+    <h1>
+      <span style="color: var(--primary);">
+        {Hiragana[charIndex].example.jp[0]}
+      </span>
+      {Hiragana[charIndex].example.jp.slice(1)}
+    </h1>
+    <p id="pronc">{Hiragana[charIndex].example.pronunciation}</p>
+    <p>{Hiragana[charIndex].example.en}</p>
+    <p>{Hiragana[charIndex].example.ar}</p>
+  </div>
+  <div id="details">
+    <h1>{Hiragana[charIndex].en.toUpperCase()}</h1>
+    <p id="phonetic">{Hiragana[charIndex].phonetic}</p>
+    <h1>{Hiragana[charIndex].ar}</h1>
+  </div>
+  <div id="controls">
+    <button on:click={back}>
+      <img style="transform: rotate(180deg);" alt="" src="/Triangle.png" />
+    </button>
+    <button on:click={next}>
+      <img alt="" src="Triangle.png" />
+    </button>
+  </div>
+</main>
+
+<style>
+  main {
+    height: 70vh;
+    width: 70vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+  }
+  #char {
+    font-size: 200px;
+  }
+  #example {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+  }
+  #example #pronc,
+  #details #phonetic {
+    margin-left: 20px;
+    margin-top: -10px;
+    color: darkgrey;
+    font-size: small;
+  }
+  #details {
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+  #controls {
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    flex-direction: row;
+    gap: 20px;
+  }
+
+  #controls button {
+    width: 50px;
+    height: 50px;
+    border-radius: 999px;
+    background-color: var(--light-primary);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  #controls button img {
+    width: 14px;
+  }
+</style>
