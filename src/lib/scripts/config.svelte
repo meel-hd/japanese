b0fa3c0fb58b49884454c1ed54ee22acc16a12e0
Mehdi ELOUALY <92476321+meel-hd@users.noreply.github.com>
2023-07-07 15:08:21 +0100

    Fix auto theme bug;

--- src/lib/scripts/config.svelte
+++ src/lib/scripts/config.svelte
@@ -7,19 +7,17 @@
 
   // Theme
   let theme = localStorage.getItem("theme");
-  if (theme) {
-    if (theme == "dark") {
-      document.body.classList.add("dark-mode");
-    } else if (theme == "light") {
-      document.body.classList.remove("dark-mode");
-    } else if (theme == "auto") {
-      autoTheme();
-      // Watch for future change
-      setInterval(() => {
-        if (localStorage.getItem("theme") == "auto") {
-          autoTheme();
-        }
-      }, 1100);
-    }
+  if (theme == "dark") {
+    document.body.classList.add("dark-mode");
+  } else if (theme == "light") {
+    document.body.classList.remove("dark-mode");
+  } else {
+    autoTheme();
+    setInterval(() => {
+      let updatedTheme = localStorage.getItem("theme");
+      if (updatedTheme != "light" && updatedTheme != "dark") {
+        autoTheme();
+      }
+    }, 1100);
   }
 </script>
